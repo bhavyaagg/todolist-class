fd790a6701c9df04e8a21e44de8257f5c65f74a1
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-09-16 15:28:21 +0530

    Delete And Checkbox Working
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- js/todolist.js
+++ js/todolist.js
@@ -2,6 +2,7 @@
  * Created by bhavyaagg on 16/9/17.
  */
   let list= null;
+  let listElements = [];
 
 window.onload = function () {
   list = document.getElementById('list');
@@ -9,17 +10,78 @@ window.onload = function () {
   let addBtn = document.getElementById('add-btn');
   let delBtn = document.getElementById('del-btn');
 
+
   addBtn.onclick = function () {
     let todoValue = addNewTodo.value;
+    addTodo(todoValue);
+    showTodos();
+    
+  }
+  
+  function showTodos() {
+    list.innerHTML="";
+    for(i in listElements){
+      addListItem(listElements[i].task,listElements[i].done,i);
+    }
+  }
 
+  function addListItem(todoValue,done,id){
     let newListItem = document.createElement('li');
+    newListItem.setAttribute('data-id',id);
     newListItem.className = 'list-group-item';
-    newListItem.innerText = todoValue;
+
+    let checkBox = document.createElement('input');
+    checkBox.className = 'col-1'
+    checkBox.setAttribute('type','checkbox');
+    checkBox.onchange = strikeSpan;
+
+    let span = document.createElement('span');
+    span.className = 'col-8'
+    span.innerText = todoValue;
+    if(done){
+      checkBox.setAttribute('checked',true);
+      span.style.textDecoration = 'line-through';
+    }
+
+
+    let deleteBtn = document.createElement('i');
+    deleteBtn.className = 'col-1 fa fa-times'
+    deleteBtn.onclick = deleteTodo;
+
+    let moveUpBtn = document.createElement('i');
+    moveUpBtn.className = 'col-1 fa fa-chevron-up'
+
+    let moveDownBtn = document.createElement('i');
+    moveDownBtn.className = 'col-1 fa fa-chevron-down'
+
+    newListItem.appendChild(checkBox);
+    newListItem.appendChild(span);
+    newListItem.appendChild(deleteBtn);
+    newListItem.appendChild(moveUpBtn)
+    newListItem.appendChild(moveDownBtn);
 
     list.appendChild(newListItem);
 
   }
 
+  function addTodo(todoTask) {
+    let newTask = {
+      task: todoTask,
+      done: false
+    }
 
+    listElements.push(newTask);
+  }
 
+  function deleteTodo(event) {
+    let index = event.target.parentElement.getAttribute('data-id');
+    listElements.splice(index,1);
+    showTodos();
+  }
+  
+  function strikeSpan(event) {
+    let index = event.target.parentElement.getAttribute('data-id');
+    listElements[index].done = event.target.checked;
+    showTodos();
+  }
 }
